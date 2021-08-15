41f4e7308e8e48acbbb5b2b927ed516b9f97d124
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2021-08-15 22:25:53 +0530

    Torch Pre-trainer

--- models/RBM/RBM_classifier.py
+++ models/RBM/RBM_classifier.py
@@ -1,8 +1,11 @@
 import numpy as np
 from .RBM_base import RBM_Model
+import tensorflow as tf
+import torch
+from tqdm import tqdm
 
 class Pre_trainer:
-	def __init__(self, n_visible, n_hidden, lr=0.001, optim='adam', k=5, batch_size=32, epochs=100):
+	def __init__(self, n_visible, n_hidden, n_classes, lr=0.001, optim='adam', k=5, batch_size=32, epochs=100):
 		self.n_hidden = n_hidden
 		self.n_visible = n_visible
 		self.lr = lr
@@ -10,10 +13,62 @@ class Pre_trainer:
 		self.k = k
 		self.batch_size = batch_size
 		self.epochs = epochs
+		self.epsilon=1e-06
+		self.n_classes = n_classes
 
 		self.rbm = RBM_Model(n_visible, n_hidden, lr, optim, k, batch_size, epochs)
-		self.model = None
+		self.model, self.W, self.vb, self.hb = None, None, None, None
 
 	def train_rbm(self, x):
 		self.model = self.rbm.train(x)
+		self.set_model(self.model)
 		return self.model
+
+	def set_model(self, model):
+		self.W = model['W']
+		self.vb = model['vb']
+		self.hb = model['hb']
+
+	def get_torch_model(self):
+		return torch.nn.Sequential(
+			torch.nn.Linear(self.n_visible, self.n_hidden),
+			torch.nn.Sigmoid(),
+			torch.nn.Linear(self.n_hidden, self.n_classes),
+			)
+
+	def set_torch_model(self):
+		model = self.get_torch_model()
+		for param, wt in zip(model.parameters(), [self.W, self.hb.flatten()]):
+			param.data = torch.from_numpy(wt)
+		return model
+
+	def fine_tune_torch(self, model, x, y):
+		x = (x - np.min(x))/(np.max(x) - np.min(x) + self.epsilon)
+		x = x.astype(np.float32)
+		x = [x[index*self.batch_size:(index+1)*self.batch_size] for index in range(len(x)//self.batch_size)]
+		y = [y[index*self.batch_size:(index+1)*self.batch_size] for index in range(len(y)//self.batch_size)]
+		criterion = torch.nn.CrossEntropyLoss()
+		optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
+		for epoch in range(self.epochs):
+			losses, accuracies = [], []
+			bar = tqdm(enumerate(zip(x, y)), total=len(x))
+			for batch_idx, (batch_x, target) in bar:
+				batch_x = torch.from_numpy(batch_x)
+				target = torch.from_numpy(target)
+				optimizer.zero_grad()
+				output = model(batch_x)
+				loss = criterion(output, target)
+				loss.backward()
+				optimizer.step()
+				preds = torch.argmax(output, 1)
+				acc = torch.mean((preds==target).float())
+				accuracies.append(acc.item())
+				losses.append(loss.item())
+				bar.set_description(str({'epoch': epoch, 'loss': round(sum(losses)/len(losses), 4), 'acc': round(sum(accuracies)/len(accuracies), 4)}))
+				bar.refresh()
+			bar.close()
+		return model
+
+	def get_tensorflow_model(self):
+		# To Be Developed
+		pass
--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from examples.data import get_data
 from models.RBM.RBM_classifier import Pre_trainer as Pre_trainer_classifier
 from models.RBM.RBM_synthetic_generator import Pre_trainer as Pre_trainer_synthetic_generator
@@ -11,7 +13,6 @@ def RBM_synthetic_generation_example(labels, images):
 	synthetic_images, hidden_features = rbm.get_synthetic_data(images_reshaped)
 	synthetic_images = np.reshape(synthetic_images, (-1, 28, 28))
 	hidden_features = np.reshape(hidden_features, (-1, 5, 2))
-	
 	plt.cla()
 	fig = plt.figure(figsize = (15,60))
 	for digit in range(10):
@@ -27,7 +28,19 @@ def RBM_synthetic_generation_example(labels, images):
 		plt.imshow(synthetic_images[index], cmap='gray')
 	fig.tight_layout()
 	plt.savefig('RBM_synthetic_generations.png')
+
+def RBM_classification_example(labels, images):
+	images_reshaped = images.reshape((len(images), -1))
+	rbm = Pre_trainer_classifier(n_visible=784, n_hidden=400, n_classes=10, epochs=15, optim='adam')
+	rbm.train_rbm(images_reshaped)
+	print("------Without Pre Training------")
+	model = rbm.get_torch_model()
+	rbm.fine_tune_torch(model, images_reshaped, labels)
+	print("-------With Pre Training--------")
+	model = rbm.set_torch_model()
+	rbm.fine_tune_torch(model, images_reshaped, labels)
 	
 if __name__ == '__main__':
 	data = get_data("./data/train.csv")
-	RBM_synthetic_generation_example(data['labels'], data['images'])
+	#RBM_synthetic_generation_example(data['labels'], data['images'])
+	RBM_classification_example(data['labels'], data['images'])
